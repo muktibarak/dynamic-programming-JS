8fe2afd406a4e6191c2aa56500119f18cbbb1384
muktibarak <muktibarak@gmail.com>
2021-01-12 16:21:51 -0500

    1 file modified

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ import "./style.css";
 // Write Javascript code!
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>grid traveller 2d array</h1>`;
+/**
+ * Problem 1:
+ * Say that you are a traveller on a 2D grid. You begin in the top-left
+ * corner and your goal is to travel to the bottom-right corner.
+ * You may only move down or right.
+ * In how many ways can you travel to the goal on a grid with dimensions
+ * m * n ?
+ */
 
 const gridTraveller = (m, n, memo = {}) => {
   // are these args in the memo?
@@ -34,3 +42,75 @@ console.log(gridTraveller(47, 56));
  * - add a base case to return memo value
  * - store return values into the memo
  */
+
+/**
+ * Problem 2:
+ * Write a function 'canSum(targetSum, numbers)' that takes in a targetSum and
+ * an array of numbers as arguments.
+ * The function should return a boolean indicating whether or not it is
+ * possible to generate the targetSum using numbers from the array.
+ * You may use an element of an array as many times as needed.
+ * You also may assume that all the input numbers are non negative.
+ * eg: canSum(7, [5, 3, 4, 7]) ---> true //as 4 + 7 = 7
+ * eg: canSum(7, [2, 3]) ---> true //as 2+2+3 = 7
+ * eg: canSum(7, [2, 4]) ---> false // as there is no way we can add and get 7
+ *
+ */
+
+const canSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
+  // lets iterate over numbers array
+
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    if (canSum(remainder, numbers, memo) === true) {
+      memo[targetSum] = true;
+      return true;
+    }
+  }
+  memo[targetSum] = false;
+  return false;
+};
+
+console.log(canSum(7, [2, 3]));
+console.log(canSum(7, [5, 3, 4]));
+console.log(canSum(7, [2, 4]));
+console.log(canSum(8, [2, 3]));
+console.log(canSum(307, [44, 34, 33]));
+
+/**
+ * Problem 3:
+ * Write a function 'howSum(targetSum, numbers)' that takes in a
+ * targetSum and an array of numbers as args.
+ * The function should return an array containing any combination of the
+ * elements that add up to exactly the targetSum, then return null.
+ * If there are multiple combinations possible, you may return any single one.
+ *
+ * howSum(7, [5, 3, 4, 7]) ---> [7] or [3, 4]
+ * howSum(8, [2, 3, 5]) ---> [3, 5] or [2, 2, 2, 2]
+ */
+
+const howSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    const remainderResult = howSum(remainder, numbers, memo);
+    if (remainderResult !== null) {
+      memo[targetSum] = [...remainderResult, num];
+      return memo[targetSum];
+    }
+  }
+  memo[targetSum] = null;
+  return null;
+};
+
+console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3, 4]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3]));
+console.log(howSum(307, [7, 14]));
